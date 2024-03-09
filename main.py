aaf96d09ecaa77f33af5c3642da73c32c06b16d5
Core447 <jmulmann@protonmail.com>
2024-03-09 13:50:45 +0100

    Update github urls to fit organization

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ class MediaPlugin(PluginBase):
 
         self.register(
             plugin_name=self.lm.get("plugin.name"),
-            github_repo="https://github.com/Core447/MediaPlugin",
+            github_repo="https://github.com/StreamController/MediaPlugin",
             plugin_version="1.0.0",
             app_version="1.0.0-alpha"
         )
